5f3cf51bd5403faa51cead297efc2200e4707e12
Enrique Sosa <elasosajr@gmail.com>
2022-05-19 16:06:26 -0700

    catching up on stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+const h1 = document.querySelector('h1');
+h1.innerText = "Goodbye World!";
+
+const allLis = document.querySelectorAll('li');
+
+for(let lis of allLis) {
+    console.log(lis);
+}
+
+const myColors = ['red', 'green', 'blue'];
+
+for(let i = 0; i < myColors.length; i++) {
+    allLis[i].style.backgroundColor = myColors[i];
+}
