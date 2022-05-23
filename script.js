b2de5b158aa118349a16c6e1da40b7317594d346
Enrique Sosa <elasosajr@gmail.com>
2022-05-23 14:05:29 -0700

    added more js and some html

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ h1.innerText = "Goodbye World!";
 
 const allLis = document.querySelectorAll('li');
 
-for(let lis of allLis) {
-    console.log(lis);
-}
-
-const myColors = ['red', 'green', 'blue'];
+const myColors = ['red', 'green', 'blue', 'yellow'];
 
 for(let i = 0; i < myColors.length; i++) {
     allLis[i].style.backgroundColor = myColors[i];
 };
+
+const addItems = document.querySelector("#addItems");
+const itemsUl = document.querySelector("#items");
+
+addItems.addEventListener('keypress', function(e) {
+    if(e.key === "Enter") {
+        const lis = document.createElement('li');
+        lis.innerText = this.value;
+        itemsUl.append(lis);
+        this.value = '';
+    }
+});
